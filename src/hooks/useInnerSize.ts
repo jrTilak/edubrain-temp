7493dbaee008c2a0c9bd154362f68a617e52997f
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-15 16:18:06 +0545

    Refactor useInnerSize hook to update size on window resize

--- src/hooks/useInnerSize.ts
+++ src/hooks/useInnerSize.ts
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 
 export const useInnerSize = () => {
-    const [size, setSize] = useState({
+  const [size, setSize] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setSize({
         height: window.innerHeight,
-        width: window.innerWidth
+        width: window.innerWidth,
+      });
     });
-    useEffect(()=>{
-        window.addEventListener(("resize"),()=>{
-            setSize({
-                height: window.innerHeight,
-        width: window.innerWidth
-            });
-        })
-    })
-    return size;
-}
+    return () => {
+      window.removeEventListener("resize", () => {
+        setSize({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        });
+      });
+    };
+  }, []);
+  return size;
+};
