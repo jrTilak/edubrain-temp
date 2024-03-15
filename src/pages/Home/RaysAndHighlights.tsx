cdc101fbcdecaf596ae9a7c76a10f95645ae4cb7
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-15 16:47:49 +0545

    some changes

--- src/pages/Home/RaysAndHighlights.tsx
+++ src/pages/Home/RaysAndHighlights.tsx
@@ -9,7 +9,6 @@ import RaysLine8 from "@/assets/icons/rays-line-8.svg";
 const RaysAndHighlights = () => {
   return (
     <>
-      {" "}
       <img
         src={RaysLine1}
         alt=""
@@ -18,37 +17,37 @@ const RaysAndHighlights = () => {
       <img
         src={RaysLine2}
         alt=""
-        className="absolute w-[142px] h-[79.38px] right-0 top-[520px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[142px] h-[79.38px] right-0 top-[500px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine3}
         alt=""
-        className="absolute w-[142px] h-[79.38px] right-0 top-[780px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[137px] h-[66px] right-0 top-[780px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine4}
         alt=""
-        className="absolute w-[142px] h-[79.38px] left-0 top-[2120px] sm:top-[2300px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[175px] h-[46px] left-0 top-[2110px] sm:top-[2300px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine5}
         alt=""
-        className="absolute w-[142px] h-[79.38px] right-0 top-[2520px] sm:top-[2950px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[175.35px] h-[46.83px] right-0 top-[2525px] sm:top-[2950px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine6}
         alt=""
-        className="absolute w-[142px] h-[79.38px] right-0 top-[8280px] sm:top-[8100px] xl:top-[7100px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[142px] h-[79.38px] right-0 top-[8380px] sm:top-[8100px] xl:top-[7100px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine7}
         alt=""
-        className="absolute w-[142px] h-[79.38px] left-0 top-[9650px] sm:top-[9500px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[109px] h-[59.38px] left-0 top-[9860px] sm:top-[9500px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
       <img
         src={RaysLine8}
         alt=""
-        className="absolute w-[142px] h-[79.38px] right-0 top-[9860px] sm:top-[9700px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
+        className="absolute w-[93.66px] h-[32.55px] right-0 top-[10130px] sm:top-[9700px] sm:w-[237px] sm:h-[132px] xl:w-[339.5px] xl:h-[189px]"
       />
     </>
   );
